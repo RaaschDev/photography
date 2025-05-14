ca55173475b4010055fe493fd15d1c48bc4c380b
Raphael <devraasch@gmail.com>
2025-05-14 11:33:32 -0300

    first commit

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,64.23.230.0').split(',')
+ALLOWED_HOSTS = ['*','64.23.230.0','127.0.0.1','localhost']
 
 
 # Application definition
